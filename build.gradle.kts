ac32c29e1bbd76ddc5c7723e0ae481dacbe26ad3
Daniel García <dany.el553@gmail.com>
2023-11-06 22:28:14 -0600

    Add base application

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.daggerHilt) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinKapt) apply false
 }
